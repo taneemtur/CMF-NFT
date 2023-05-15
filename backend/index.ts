d41338042daecd4c77abafc7136363336f7b5b87
Taneem UR Rehman <taneem122@gmail.com>
2023-05-15 19:32:10 +0100

    CORS

--- backend/index.ts
+++ backend/index.ts
@@ -8,7 +8,22 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+const whitelist = ["http://localhost:3000", "https://superex-nft.vercel.app/", "*"]
+const corsOptions = {
+  origin: function (origin:any, callback:any) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+
+app.use(cors({
+  origin: '*'
+}));
+// app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
