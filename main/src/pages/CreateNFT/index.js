6e7094392f9555c52b02115ae81efcba0fb53338
usamakashif <usamakashif1@gmail.com>
2023-05-25 00:19:30 +0500

    sendinf token id to backend

--- main/src/pages/CreateNFT/index.js
+++ main/src/pages/CreateNFT/index.js
@@ -161,7 +161,7 @@ const CreateNFT = () => {
         const id = toast.loading('Creating Item');
         const tokenId = mintItem.data.events.TokensMinted.returnValues.tokenIdMinted;
         console.log('token', tokenId)
-        data.tokenID = tokenId;
+        data["tokenID"] = "helloworld";
         formData.append('data', JSON.stringify(data))
         await axiosConfig.post("/nfts/createnft", formData, {
           body: data,
