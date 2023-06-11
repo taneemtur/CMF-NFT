3fb19f5263c3f869bbac5244b47c14a46f63670c
ahmedzafar0 <talhazafar0@gmail.com>
2023-06-11 21:31:07 +0500

    updated backend

--- main/src/axiosConfig.js
+++ main/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const locallhost = 'http://localhost:8080';
 // https://cmfnft.vercel.app
 const instance = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://cmf-api.vercel.app"
 });
 // set content type to json
 instance.defaults.headers.post['Content-Type'] = 'application/json';
